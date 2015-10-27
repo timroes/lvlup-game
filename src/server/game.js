4aa885e9e328aa4f04ddd5bbfc7ca0212110bf84
Tim Roes <mail@timroes.de>
2015-10-27 20:43:25 +0100

    refactor: make question its own class

--- src/server/game.js
+++ src/server/game.js
@@ -1,17 +1,17 @@
-const q = require('q'),
+	const q = require('q'),
 	server = require('./server'),
 	socketio = require('socket.io');
 
+import Question from './model/question';
 import Player from './player';
 import * as utils from './utils';
+
+const timeOverrun = 2; // in seconds
 
 const states = {
 	waiting: Symbol("waiting"),
 	question: Symbol("question")
 };
-
-const timePerQuestion = 30; // in seconds
-const timeOverrun = 2; // in seconds
 
 export default class Game {
 
@@ -20,7 +20,7 @@ export default class Game {
 		this.state = states.waiting;
 		this.players = {};
 		this.usernames = [];
-		this.questions = require('./questions');
+		this.questions = require('./demo-questions');
 		this.currentQuestion = null;
 		this.initSocket();
 	}
@@ -37,10 +37,9 @@ export default class Game {
 				if (player) {
 					socket.player = player;
 					player.socket = socket;
-					console.log(player.infos);
 					socket.emit('player', player.infos);
 					if (this.currentQuestion) {
-						socket.emit('question', this.currentQuestion.json, this.currentQuestion.endTime - Date.now());
+						socket.emit('question', this.currentQuestion.clientJson, this.currentQuestion.timeRemaining);
 					}
 					if (player.currentAnswer) {
 						socket.emit('answer-chosen', player.currentAnswer);
@@ -89,51 +88,39 @@ export default class Game {
 
 	setQuestion(id) {
 		if (this.currentQuestion) {
-			clearTimeout(this.currentQuestion.timeout);
+			clearTimeout(this.currentQuestionTimeoutId);
 		}
 
-		let question = this.questions[id];
-		// Transform the object to the version, the clients should get
-		let answers = [question.answer, ...question.wrongAnswers].map((a) => { return { id: utils.generateUUID(), answer: a }; });
-		let correctId = answers[0].id;
-		utils.shuffle(answers);
-		const questionForClients = {
-			type: question.type,
-			question: question.question,
-			answers: answers,
-			exp: question.exp
-		};
+		if (!this.questions[id]) {
+			throw new Error(`Cannot find question with id ${id}.`);
+		}
 
-		let endTime = Date.now() + timePerQuestion * 1000;
-		this.io.sockets.emit('question', questionForClients, timePerQuestion * 1000);
-		// End the current question 2 seconds after it should actually end (to
-		// compensate bad network, etc.)
-		let timeout = setTimeout(this.endQuestion.bind(this), (timePerQuestion + timeOverrun) * 1000);
+		let question = Question.parse(this.questions[id]);
 
-		this.currentQuestion = {
-			question: question,
-			timeout: timeout,
-			endTime: endTime,
-			correctId: correctId,
-			json: questionForClients,
-			exp: question.exp
-		};
+		let remainingTime = question.start();
+		this.io.sockets.emit('question', question.clientJson, remainingTime);
+		// End the current question 2 seconds after it should actually end (to compensate bad network, etc.)
+		this.currentQuestionTimeoutId = setTimeout(this.endQuestion.bind(this), remainingTime + timeOverrun * 1000);
+
+		this.currentQuestion = question;
 	}
 
 	endQuestion() {
 		if (!this.currentQuestion) return;
-		console.log("end current question");
-		if (this.currentQuestion.timeout) {
-			clearTimeout(this.currentQuestion.timeout);
+
+		if (this.currentQuestionTimeoutId) {
+			clearTimeout(this.currentQuestionTimeoutId);
 		}
-		let winExp = this.currentQuestion.exp;
-		let loseExp = -Math.ceil(this.currentQuestion.exp * 0.5);
 
 		for (let id in this.players) {
 			let player = this.players[id];
-			player.lvlup(player.currentAnswer.id === this.currentQuestion.correctId ? winExp : loseExp);
+			if (player.currentAnswer) {
+				// player.lvlup(player.currentAnswer.id === this.currentQuestion.correctId ? winExp : loseExp);
+				let exp = this.currentQuestion.expForAnswer(player.currentAnswer);
+				player.lvlup(exp);
+				player.socket.emit('player:update', player.lvlInfos);
+			}
 			// TODO: send correct or wrong answers
-			player.socket.emit('player:update', player.lvlInfos);
 			player.currentAnswer = null;
 		}
 
---
+++ src/server/model/question.js
@@ -0,0 +1,90 @@
+import * as utils from '../utils';
+
+const timePerQuestion = 30; // in seconds
+
+export default class Question {
+
+	/**
+	 * Create a question from the JSON imported from a question file.
+	 */
+	static parse(json) {
+		if (json.type === 'choice') {
+			return new ChoiceQuestion(json);
+		} else {
+			throw new Error(`Cannot generate question of unknown type '${json.type}'.`)
+		}
+	}
+
+	constructor(json) {
+		if (!json.type || !json.exp) {
+			throw new Error(`Not all required attributes in parsing JSON.`);
+		}
+		this.type = json.type;
+		
+		this.exp = json.exp;
+		this.winExp = this.exp;
+		this.loseExp = -Math.ceil(this.exp * 0.5);
+
+		this._json = json;
+	}
+
+	/**
+	 * Returns the JSON object, that should be transmitted to the client.
+	 * This should not contain any solution information anymore.
+	 */
+	get clientJson() {
+		return {
+			type: this.type,
+			exp: this.exp
+		};
+	}
+
+	get timeRemaining() {
+		return Math.max(0, this.endTime - Date.now());
+	}
+
+	start() {
+		let remainingTime = timePerQuestion * 1000;
+		this.endTime = Date.now() + remainingTime;
+		return remainingTime;
+	}
+
+	expForAnswer(answer) {
+		return this.validateAnswer(answer) ? this.winExp : this.loseExp;
+	}
+
+	validateAnswer(answer) {
+		return false;
+	}
+
+}
+
+class ChoiceQuestion extends Question {
+
+	constructor(json) {
+		super(json);
+
+		this.question = json.question;
+
+		this.answers = [json.answer, ...json.wrongAnswers].map((a) => {
+			return {
+				id: utils.generateUUID(),
+				answer: a
+			};
+		});
+		this.correctId = this.answers[0].id;
+		utils.shuffle(this.answers);
+	}
+
+	get clientJson() {
+		let json = super.clientJson;
+		json.question = this.question;
+		json.answers = this.answers;
+		return json;
+	}
+
+	validateAnswer(answer) {
+		return answer.id === this.correctId;
+	}
+
+}
