346a1c0cc1904f51cb197c504eb7b8e1d2d9fda7
Tim Roes <mail@timroes.de>
2015-11-02 21:12:04 +0100

    feat: show error messages on login screen

--- src/modules/client/scripts/controllers/LoginController.js
+++ src/modules/client/scripts/controllers/LoginController.js
@@ -1,14 +1,26 @@
 angular.module('lvlup.client')
 .controller('LoginController', function($location, game) {
 
+	var ctrl = this;
+
 	if (game.getSession()) {
 		$location.path('/game');
 	}
 
-	this.login = function() {
+	ctrl.login = function() {
+		ctrl.error = null;
 		game.login(this.name)
 			.then(function() {
 				$location.path('/game');
+			})
+			.catch(function(reason) {
+				if (reason.status === 409) {
+					ctrl.error = 'duplicateUsername';
+				} else if(reason.status === 400) {
+					ctrl.error = 'invalidUsername';
+				} else {
+					ctrl.error = 'generalError';
+				}
 			});
 	};
 
