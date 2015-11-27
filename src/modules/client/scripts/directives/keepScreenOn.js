e5abb9dc1ffebc0dcc22bde6ed6af9646723ab5c
Tim Roes <mail@timroes.de>
2015-11-27 22:40:44 +0100

    feat: improve styling of keep screen on

--- src/modules/client/scripts/directives/keepScreenOn.js
+++ src/modules/client/scripts/directives/keepScreenOn.js
@@ -2,7 +2,7 @@ angular.module('lvlup.client')
 .directive('keepScreenOn', function($window) {
 	return {
 		restrict: 'E',
-		template: '<div class="keepscreenon" ng-class="{ \'keepscreenon--enabled\': isKeepOn }"><video class="keepscreenon__video" controls loop muted><source type="video/webm" src="data:video/webm;base64,GkXfowEAAAAAAAAfQoaBAUL3gQFC8oEEQvOBCEKChHdlYm1Ch4ECQoWBAhhTgGcBAAAAAAAByBFNm3RALE27i1OrhBVJqWZTrIHfTbuMU6uEFlSua1OsggEwTbuMU6uEHFO7a1OsggGr7AEAAAAAAACkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAVSalmAQAAAAAAAEUq17GDD0JATYCNTGF2ZjU2LjI1LjEwMVdBjUxhdmY1Ni4yNS4xMDFzpJDspePG9gGwYQj25uHJdXqORImIQECAAAAAAAAWVK5rAQAAAAAAADuuAQAAAAAAADLXgQFzxYEBnIEAIrWcg3VuZIaFVl9WUDiDgQEj44OEAfygVeABAAAAAAAABrCBAbqBAR9DtnUBAAAAAAAAKOeBAKOjgQAAgBACAJ0BKgEAAQAARwiFhYiFhIgCAgAMDWAA/v+j3gAcU7trAQAAAAAAABG7j7OBALeK94EB8YIBd/CBAw=="></video><button class="keepscreenon__button" ng-click="keepOn()" ng-class="{ \'keepscreenon__button--hidden\': !isAndroidChrome }"><i class="icon-sun"></i> Keep screen on while playing</div>',
+		template: '<div class="keepscreenon" ng-class="{ \'keepscreenon--enabled\': isKeepOn || !isAndroidChrome }"><video class="keepscreenon__video" controls loop muted><source type="video/webm" src="data:video/webm;base64,"></video><button class="keepscreenon__button" ng-click="keepOn()"><i class="icon-sun"></i> Keep screen on while playing</div>',
 		replace: true,
 		link: function(scope, elem) {
 			var video = elem.find('video')[0];
